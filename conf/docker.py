3f70b92070a5a39ca831ea0882bbcf025da9bbf6
Antons Kranga <anton@agilestacks.com>
2019-11-12 22:14:42 -0800

    /assets goes to /static as per flask naming convention

--- conf/docker.py
+++ conf/docker.py
@@ -1,6 +1,6 @@
 import os
 DEBUG           = True
 SOURCE          = os.getenv('SOURCE') or 0
-FALLBACK_SOURCE = '/app/assets/test_video.mp4'
+FALLBACK_SOURCE = '/app/static/test_video.mp4'
 FRAMES          = int( os.getenv('FRAMES', 24))
 JSON_ADD_STATUS = False
